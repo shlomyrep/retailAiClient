4808bcddc60edb63596b79dc41c53fe9e3bb6e8c
Shlomy Nackson <mailtoshlomy@gmail.com>
2024-04-02 09:17:00 +0300

    added simulator

--- shared/src/commonMain/kotlin/business/constants/KtorConstants.kt
+++ shared/src/commonMain/kotlin/business/constants/KtorConstants.kt
@@ -1,4 +1,4 @@
 package business.constants
 
-//const val BASE_URL = "http://10.0.2.2:3000/api/"
-const val BASE_URL = "http://127.0.0.1:3000/api/"
+const val BASE_URL = "http://10.0.2.2:3000/api/" //for android emulator
+//const val BASE_URL = "http://127.0.0.1:3000/api/" //for iPhone Simulator
