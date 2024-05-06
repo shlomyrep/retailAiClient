79d9730d3a32c739e5d89d187b901964f9510bf0
Tamir Nahum <tamir.n@appnext.com>
2024-05-06 14:05:28 +0300

    remove Companion object from detail view model and added DetailTexts class instead

---
+++ shared/src/commonMain/kotlin/presentation/ui/main/detail/view_model/DetailTexts.kt
@@ -0,0 +1,10 @@
+package presentation.ui.main.detail.view_model
+
+class DetailTexts {
+    val inventoryUpdate = "inventoryUpdate"
+    val inventoryNotAvailable = "inventoryNotAvailable"
+    val singleInventoryResult = "singleInventoryResult"
+    val yes = "yes"
+    val no = "no"
+    val inventoryList = "inventoryList"
+}
