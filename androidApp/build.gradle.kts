c84a1a555937df16bda666c03dc5d8dcabce3ad2
Tamir Nahum <tamir.n@appnext.com>
2025-03-03 12:24:10 +0200

    upgrade version code

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "com.digitup.retailai.android"
         minSdk = (findProperty("android.minSdk") as String).toInt()
         targetSdk = (findProperty("android.targetSdk") as String).toInt()
-        versionCode = 1003
-        versionName = "1.3"
+        versionCode = 1004
+        versionName = "1.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
