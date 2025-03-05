9d94a25f533af985ca55afc30eb263e1d9008402
Tamir Nahum <tamir.n@appnext.com>
2025-03-05 16:56:51 +0200

    VER 1.8

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "com.digitup.retailai.android"
         minSdk = (findProperty("android.minSdk") as String).toInt()
         targetSdk = (findProperty("android.targetSdk") as String).toInt()
-        versionCode = 1007
-        versionName = "1.7"
+        versionCode = 1008
+        versionName = "1.8"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
